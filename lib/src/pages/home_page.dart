4e1df0e4328ee8b961be889a1e9ff7ff4044cd0f
Juan de Dios Gomez F <jddios837@gmail.com>
2020-02-01 14:03:16 -0500

    Finished save settings wuth Preferences

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,24 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:settings_app/src/share_prefs/preferencias_usuario.dart';
+
+import 'package:settings_app/src/widgets/menu_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  
+  static final String routeName = 'home';
+  final prefs = new PreferenciasUsuarios();
 
   @override
   Widget build(BuildContext context) {
+
+    prefs.ultimaPagina = HomePage.routeName;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Preferencias de Usuario'),
+        backgroundColor: (prefs.colorSecundario) ? Colors.teal : Colors.blue,
       ),
+      drawer: MenuWidget(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text('Color secundario:'),
+          Text('Color secundario: ${ prefs.colorSecundario }'),
           Divider(),
-          Text('Genero:'),
+          Text('Genero: ${ prefs.genero }'),
           Divider(),
-          Text('Nombre de usuario:'),
+          Text('Nombre de usuario: ${ prefs.nombre }'),
           Divider(),
         ],
       ),
     );
   }
+
+  // Widget _crearMenu(BuildContext context) {
+  //     return Drawer(
+  //       child: ListView(
+  //         padding: EdgeInsets.zero,
+  //         children: <Widget>[
+  //           DrawerHeader(
+  //             child: Text('Mi Menú'),
+  //             decoration: BoxDecoration(
+  //               image: DecorationImage(
+  //                 image: AssetImage('assets/menu-img.jpg'),
+  //                 fit: BoxFit.cover
+  //               )
+  //             ),
+  //           ),
+  //           ListTile(
+  //             leading: Icon(Icons.pages, color: Colors.blue,),
+  //             title: Text('Pages'),
+  //             onTap: (){
+  //               Navigator.pushReplacementNamed(context, HomePage.routeName);
+  //             },
+  //           ),
+  //           ListTile(
+  //             leading: Icon(Icons.party_mode, color: Colors.blue,),
+  //             title: Text('Party'),
+  //             onTap: (){},
+  //           ),
+  //           ListTile(
+  //             leading: Icon(Icons.people, color: Colors.blue,),
+  //             title: Text('People'),
+  //             onTap: (){},
+  //           ),
+  //           ListTile(
+  //             leading: Icon(Icons.settings, color: Colors.blue,),
+  //             title: Text('Settings'),
+  //             onTap: (){
+  //               // Navigator.pop(context);
+  //               // Navigator.pushNamed(context, SettingsPage.routeName);
+  //               Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+  //             },
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   }
 }
